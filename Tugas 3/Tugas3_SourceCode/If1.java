5469e18e662e3db07c2a3cb7dfe49cd20319ec52
Rahman Ramadhan <161214072+rahman2377@users.noreply.github.com>
2024-03-13 22:50:43 +0800

    Add files via upload

---
+++ Tugas 3/Tugas3_SourceCode/If1.java
@@ -0,0 +1,28 @@
+/*Nama : Rahman Ramadhan
+Nim : 13020220128
+Tanggal : Rabu,3-13-2024
+ */
+
+/* contoh pemakaian IF satu kasus */
+/* membaca nilai integer, menuliskan nilainya jika positif */
+
+import java.util.Scanner;
+
+public class If1 {
+    /**
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        Scanner masukan=new Scanner(System.in);
+        int a;
+        /* Program */
+        System.out.print ("Contoh IF satu kasus \n");
+        System.out.print ("Ketikkan suatu nilai integer : ");
+        a = masukan.nextInt();
+        if (a >= 0)
+            System.out.print ("\nNilai a positif "+ a);
+    }
+}
---
+++ Tugas 3/Tugas3_SourceCode/KasusBoolean.java
@@ -0,0 +1,27 @@
+/*Nama : Rahman Ramadhan
+Nim : 13020220128
+Tanggal : Rabu,3-13-2024
+ */
+
+/* Eksrpesi kondisional dengan boolean */
+public class KasusBoolean {
+    /**
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        boolean bool;
+        /* Algoritma */
+        bool= true;
+        if(bool) {
+            System.out.print("true\n");
+        } else
+            System.out.print("false\n");
+        if(!bool) {
+            System.out.print("salah\n");
+        } else
+            System.out.print("benar\n");
+    }
+}
